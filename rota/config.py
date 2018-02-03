a85f6affb40442b5c1b0c57d78d804cf2b594d9a
Dean Langsam <deanla@gmail.com>
2018-02-04 00:39:45 +0200

    add logging

---
+++ rota/config.py
@@ -0,0 +1,34 @@
+from itertools import product
+import numpy as np
+import logging
+import sys
+from datetime import datetime
+# Pandas print options
+np.set_printoptions(precision=4, suppress=True, linewidth=80)
+
+file_name = str(datetime.now()).replace(" ","-").replace(":","-")
+file_name = file_name[:file_name.find('.')]+'.log'
+# file_name = 'my_log.log'
+# print (file_name)
+# Logger class
+logger = logging.getLogger('pertussis')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(levelname)-s, %(message)s')
+
+sh = logging.StreamHandler()
+sh.setFormatter(formatter)
+sh.setLevel(logging.CRITICAL)
+
+fh = logging.FileHandler('./log/'+file_name, mode='a')
+fh.setFormatter(formatter)
+fh.setLevel(logging.DEBUG)
+
+
+# progress = logging.getLogger('progress')
+# pb = logging.FileHandler('./progress.log', mode='w')
+# pb.setFormatter(formatter)
+# pb.setLevel(logging.DEBUG)
+
+logger.addHandler(sh)
+logger.addHandler(fh)
+# progress.addHandler(pb)
